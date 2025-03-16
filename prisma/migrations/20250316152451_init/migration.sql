78003db32579029baf76b2e05625385e5fe16f33
trandangduat <duattran.05@gmail.com>
2025-03-16 22:30:27 +0700

    Add Prisma Postgresql

---
+++ prisma/migrations/20250316152451_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Quiz" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Quiz_pkey" PRIMARY KEY ("id")
+);
