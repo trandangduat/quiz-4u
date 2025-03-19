49fc44b7c1fd96213b4f991a22bed5b2beb7da4e
trandangduat <duattran.05@gmail.com>
2025-03-19 18:09:05 +0700

    Add quiz dynamic routes

--- app/quiz/[quizId]/page.tsx
+++ app/quiz/[quizId]/page.tsx
@@ -1,12 +1,59 @@
-export default async function Page({ 
-    params 
-} : { 
-    params: Promise<{ quizId: string }> 
-}) {
+import { QuizQuestion } from "@/types/quiz";
+import { randomUUID } from "crypto";
+import { Suspense } from "react";
+
+function Question ({ Q } : { Q: QuizQuestion }) {
+    return (
+        <div>
+            <p className="font-bold">{Q.question}</p>
+            {Q.choices.map(choice => (
+                <div key={randomUUID()}>
+                    <p>{choice}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+async function Quiz({ params } : { params: Promise<{ quizId: string }> }) {
     const { quizId } = await params;
+    const quiz = await prisma.quiz.findUnique({
+        where: {
+            id: quizId,
+        },
+        include: {
+            questions: true,
+        }
+    });
+
     return (
         <>
-            <h1>Test page {quizId}</h1>
+            <h1>{quiz?.title}</h1>
+            <div className="flex flex-col gap-8">
+                {quiz?.questions.map(Q => (
+                    <Question Q={Q} key={Q.id} />
+                ))}
+            </div>
         </>
-    );
+    )
+}
+
+function QuizSkeleton() {
+    return (
+        <>
+            <h1>Loading...</h1>
+        </>
+    )
+}
+
+export default function Page({ params } : { params: Promise<{ quizId: string }> }) {
+
+    return (
+        <>
+            <h1>Hello quiz</h1>
+            <Suspense fallback={<QuizSkeleton />}>
+                <Quiz params={params} />
+            </Suspense>
+        </>
+    )
 }
