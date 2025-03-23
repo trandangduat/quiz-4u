1b939ce8ba24cbc43f98229cc831d3d2aa70769d
trandangduat <duattran.05@gmail.com>
2025-03-23 11:13:29 +0700

    Add question interactivity for quiz

--- app/quiz/components/quiz-form.tsx
+++ app/quiz/components/quiz-form.tsx
@@ -1,34 +1,72 @@
 "use client"
 
-import { QuizQuestion } from "@/types/quiz";
-import { useState } from "react";
+import { cn } from "@/utils";
+import { Dispatch, SetStateAction, useState } from "react";
 
-function Choice({ content } : { content: string }) {
+function Choice({ content, index, questionId, isChosen, setUserChoices } : 
+    { 
+        content: string,
+        index: number,
+        questionId: number,
+        isChosen: boolean,
+        setUserChoices: Dispatch<SetStateAction<Record<string, number>>>
+    }
+) {
+
+    function updateUserAnswer() {
+        setUserChoices(prevChoices => {
+            return { ...prevChoices, [questionId]: index };
+        });
+    }
+
     return (
-        <div>
-            <p>{content}</p>
+        <div
+            className={cn("hover:bg-zinc-700", isChosen && "bg-purple-700")}
+            onClick={() => updateUserAnswer()}
+        >
+            <p>{index}. {content}</p>
         </div>
     );
 }
 
-function Question ({ Q } : { Q: QuizQuestion }) {
+function Question ({ Q, userChoices, setUserChoices } : 
+    { 
+        Q: any,
+        userChoices: Record<string, number>,
+        setUserChoices: Dispatch<SetStateAction<Record<string, number>>>
+    }
+) {
     return (
         <div>
             <p className="font-bold">{Q.question}</p>
-            {Q.choices.map(choice => (
-                <Choice key={choice} content={choice} />
-            ))}
+            <div className="flex flex-col">
+                {Q.choices.map((choice: string, index: number) => (
+                    <Choice 
+                        key={choice} 
+                        questionId={Q.id}
+                        setUserChoices={setUserChoices}
+                        content={choice} 
+                        index={index}
+                        isChosen={userChoices[Q.id] == index}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
 
 export default function QuizForm({ quiz } : { quiz: any }) {
-    const [userChoices, setUserChoices] = useState({});
+    const [userChoices, setUserChoices] = useState<Record<string, number>>({});
     return (
         <>
             <div className="flex flex-col gap-8">
-                {quiz?.questions.map(Q => (
-                    <Question Q={Q} key={Q.id} />
+                {quiz?.questions.map((Q: any) => (
+                    <Question 
+                        Q={Q} 
+                        key={Q.id} 
+                        userChoices={userChoices}
+                        setUserChoices={setUserChoices}
+                    />
                 ))}
                 <button>Submit</button>
             </div>
