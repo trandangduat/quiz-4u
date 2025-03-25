ab6f16e4316ccc82b33253b2594ccf657a2ee98a
trandangduat <duattran.05@gmail.com>
2025-03-25 22:17:02 +0700

    refactor navbar, add more colors

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,52 @@
+import { cn } from "@/lib/utils";
 import ToggleThemeButton from "./toggle-theme-button";
 import UserInfo from "./user-info";
 import Link from "next/link";
+import React from "react";
 
-export default async function NavBar() {
+const links = [
+    { title: "Home", link: "/" },
+    { title: "My quizzes", link: "/quiz" },
+];
+
+interface NavBarItemProps extends React.LiHTMLAttributes<HTMLLIElement> {
+    className?: string;
+}
+
+function NavBarItem({ className, children, ...props }: NavBarItemProps) {
     return (
-        <div className="flex flex-row gap-8 justify-center">
-            <UserInfo />
-            <Link href="/quiz">My quizzes</Link>
-            <Link href="/">Home</Link>
-            <ToggleThemeButton />
-        </div>
+        <li className={cn(className)} {...props}>
+            {children}
+        </li>
+    );
+}
+
+interface NavBarGroupProps extends React.HTMLAttributes<HTMLUListElement> {
+    className?: string;
+}
+
+function NavBarGroup({ className, children, ...props }: NavBarGroupProps) {
+    return (
+        <ul className={cn(className)} {...props}>
+            {children}
+        </ul>
+    );
+}
+
+export default function NavBar() {
+    return (
+        <nav className="flex flex-row gap-8 justify-center">
+            <NavBarGroup>
+                {links.map((item) => (
+                    <NavBarItem key={item.link}>
+                        <Link href={item.link}>{item.title}</Link>
+                    </NavBarItem>
+                ))}
+            </NavBarGroup>
+            <NavBarGroup>
+                <ToggleThemeButton />
+                <UserInfo />
+            </NavBarGroup>
+        </nav>
     );
 }
