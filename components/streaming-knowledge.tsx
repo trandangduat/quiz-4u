46fc1506a0b7b2c4c49af916670228fc4f8f0a94
trandangduat <duattran.05@gmail.com>
2025-04-10 15:35:16 +0700

    add scrollbar to streaming knowledge component, visual adjust

--- components/streaming-knowledge.tsx
+++ components/streaming-knowledge.tsx
@@ -47,10 +47,10 @@ export default function StreamingKnowledge({
         <div className="animate-slide-in">
             <div 
                 className={cn(
-                    needScroll ? "before:h-12 after:h-12" : "before:h-0 after:h-0",
-                    "bg-linear-to-tl border from-secondary to-transparent rounded-md py-4 px-6 max-h-64 overflow-hidden",
-                    "before:transition-all before:absolute before:top-0 before:left-0 before:right-0 before:bg-gradient-to-b before:from-secondary-50/80 before:to-transparent before:rounded-md",
-                    "after:transition-all after:absolute after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-t after:from-secondary-50/80 after:to-transparent after:rounded-md",
+                    needScroll ? "before:h-10 after:h-10" : "before:h-0 after:h-0",
+                    "bg-linear-to-tl border from-secondary/50 to-transparent rounded-md py-4 px-6 max-h-64 overflow-y-auto",
+                    "before:transition-all before:absolute before:top-0 before:left-0 before:right-0 before:bg-gradient-to-b before:from-secondary-200/50 before:to-transparent before:rounded-md",
+                    "after:transition-all after:absolute after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-t after:from-secondary-200/50 after:to-transparent after:rounded-md",
                     geistSans.className,
                 )}
                 ref={streamingKnowledgeRef}
