68b6c558cfcfb26e2eac1a2806e9b788d17cbb2f
trandangduat <duattran.05@gmail.com>
2025-04-28 14:59:22 +0700

    last ui polishment before fixing AI agents

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # todo
 
+- [ ] improve LLM agents result
 - [x] fix current attempt is mistaken as ongoing even though it was already submitted
 - [x] add try/catch for database operations in quiz attempt
 - [x] save current attempt into database when: starting quiz (ensure persistant even after refreshing page), every changes, user submits, out of time
-- [ ] apply quiz config when creating new quiz attempt (shuffle questions, ...)
-- [ ] allow user to see previous attempts with given config (shuffle order, timelimit, correct choices, ...)
-- [ ]
+- [x] apply quiz config when creating new quiz attempt (shuffle questions, ...)
+- [x] allow user to see previous attempts with given config (shuffle order, timelimit, correct choices, ...)
--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -60,7 +60,7 @@ export const DialogContent = ({ children, className }: DialogContentProps) => {
       }}
     >
       <div className={cn(
-        "bg-card dark:bg-secondary-100/60 border-1 backdrop-blur-xl w-full max-w-lg rounded-md p-6 flex flex-col gap-4 transition-all duration-200",
+        "bg-card dark:bg-secondary/30 border-1 backdrop-blur-xl w-full max-w-lg rounded-md p-6 flex flex-col gap-4 transition-all duration-200",
         open ? "scale-100 opacity-100" : "scale-90 opacity-0",
         className
       )}>
@@ -80,7 +80,7 @@ export const DialogHeader = ({ className, title, description }: DialogHeaderProp
   const { setOpen } = useContext(DialogContext);
 
   return (
-    <div className={cn("flex flex-row justify-between items-center gap-4 p-4 rounded-md bg-secondary/20", className)}>
+    <div className={cn("flex flex-row justify-between items-center gap-4 p-4 rounded-md bg-secondary/35", className)}>
       <div className="space-y-1.5 flex flex-col">
         {title && <span className="text-lg leading-none font-bold tracking-tight">{title}</span>}
         {description && <span className="text-sm text-muted-foreground">{description}</span>}
