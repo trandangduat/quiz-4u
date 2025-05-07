07642ace9ae829f42be251e284259f3408ba1152
trandangduat <duattran.05@gmail.com>
2025-05-07 14:17:07 +0700

    quizcard ui update x3

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -41,7 +41,7 @@ async function Quizzes() {
           </Link>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-6">
           {createdQuizzes.map((quiz) => (
             <QuizCard
               key={quiz.id}
--- components/quiz-card.tsx
+++ components/quiz-card.tsx
@@ -17,17 +17,17 @@ export default function QuizCard({ quizLink, quizInfo } : {
         <Link href={quizLink}>
             <div
                 className={cn(
-                    "group bg-card/50 dark:bg-secondary/15 rounded-md p-6 h-full relative transition-all overflow-clip duration-200 border-1 dark:border-primary/15 hover:border-primary/50 shadow-none shadow-secondary/70 hover:shadow-2xl",
-                    "after:bg-radial-[at_20%_20%] after:from-transparent after:to-primary/20 after:inset-0 after:w-full after:h-full after:absolute after:opacity-0 after:-z-1 after:transition-opacity hover:after:opacity-100",
+                    "group bg-card/50 dark:bg-secondary/15 rounded-md p-6 h-full relative transition-all duration-150 border-2 dark:border-primary/15 hover:border-primary/50 shadow-none shadow-secondary/70 hover:shadow-2xl",
+                    "after:bg-radial-[at_20%_20%] after:from-transparent after:to-primary/20 after:inset-0 after:w-full after:h-full after:absolute after:opacity-0 after:-z-2 after:transition-opacity hover:after:opacity-100",
                     "hover:bg-primary-50 dark:hover:bg-secondary/50"
                 )}
             >
-                <div className="flex items-center gap-4 justify-between">
+                <div className="relative">
                     <div className="flex flex-col items-start gap-3">
-                        <div>
-                            <span className="font-bold text-lg tracking-tight group-hover:text-primary dark:group-hover:text-foreground transition-colors">{quizInfo?.title || 'Unnamed'}</span>
+                        <div className="w-full">
+                            <p className="font-bold text-lg tracking-tight group-hover:text-primary dark:group-hover:text-foreground transition-colors overflow-hidden whitespace-nowrap text-ellipsis w-full">{quizInfo?.title || 'Unnamed'}</p>
                         </div>
-                        <div className="flex flex-wrap gap-4 text-sm text-primary dark:text-muted-foreground font-semibold">
+                        <div className="flex flex-wrap gap-4 text-sm text-primary dark:text-primary font-semibold">
                             <div className="inline-flex gap-1 items-center px-2 py-1 bg-primary-500/10 dark:bg-primary/10 rounded-md">
                                 <List size={14} />
                                 <span>{quizInfo?.questionCount || 0} questions</span>
@@ -38,7 +38,7 @@ export default function QuizCard({ quizLink, quizInfo } : {
                             </div>
                         </div>
                     </div>
-                    <div className="group-hover:opacity-100 group-hover:animate-slide-left-and-show-inf opacity-0 transition-all text-primary relative after:absolute after:inset-0 after:w-full after:bg-radial after:from-primary after:to-transparent after:-z-1 after:blur-2xl">
+                    <div className="flex items-center absolute z-10 h-full top-0 right-0 group-hover:opacity-100 group-hover:animate-slide-left-and-show-inf opacity-0 transition-all text-primary after:absolute after:inset-0 after:w-full after:bg-radial after:from-primary after:to-transparent after:-z-1 after:blur-2xl">
                         <ChevronsRight size={48} strokeWidth={2} />
                     </div>
                 </div>
